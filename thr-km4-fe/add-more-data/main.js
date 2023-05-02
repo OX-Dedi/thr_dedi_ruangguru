62c1cf6edf70648ac4a546798411cf50e72e199e
Dedi <dedir8361@gmail.com>
2023-05-03 00:57:52 +0700

    submit thr

---
+++ thr-km4-fe/add-more-data/main.js
@@ -0,0 +1,9 @@
+function addMoreData(object, key, value) {
+   const exit = object.assign(object, { [key]: value });
+
+  return exit;
+}
+
+console.log(addMoreData({ name: "Edo", age: 20 }, "address", "Jakarta"));
+console.log(addMoreData({ name: "Edo", age: 20 }, "hobies", ["coding", "reading"]));
+console.log(addMoreData({ name: "Edo", age: 20 }, "isMarried", false));
